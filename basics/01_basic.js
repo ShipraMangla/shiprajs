acdd769b3b059e4e2cd48fd73cb6a1995012240a
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-02-13 22:04:14 +0000

    container config done

---
+++ basics/01_basic.js
@@ -0,0 +1,10 @@
+const accountID=123;
+let accountName="Shipra"
+var accountPassword="01234"
+ accountId=234;
+ accountName="hitesh"
+ accountPassword="298434"
+ accountCity="delhi"
+ let accountstate;
+console.log(accountID)
+console.table([accountId,accountName,accountPassword,accountCity,accountstate])
