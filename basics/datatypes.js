98af9add95f5d1193313f1469bcc91fc6159499f
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-02-17 07:41:08 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"
+console.log(3+4)
+console.log("Shipra"); 
+console.log("hitesh")
+let name="shipra"
+let age=20
+let isAvailable=true
+//number
+//string
+//boolean
+//bigInt
+//null
+//undefined
+//symbol
+let state
+let temperature=null;
+console.table([name,age,isAvailable,state,temperature])
+
+console.log(typeof state)
