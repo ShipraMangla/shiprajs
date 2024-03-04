0307718137e468f48262e3b08424cef1fbdfddd2
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-03-04 11:11:19 +0000

    type conversion

---
+++ basics/type.js
@@ -0,0 +1,23 @@
+let age= "20"
+console.log(typeof(age))
+let AgeinNumber = Number(age)
+console.log(typeof(AgeinNumber))
+console.log(AgeinNumber)
+
+
+let score=""
+console.log(typeof(score))
+let booleanValue = Boolean(score)
+console.log(typeof(booleanValue))
+console.log(booleanValue)
+
+let AgeNumber=25
+console.log(typeof AgeNumber)
+let stringNumber=String(AgeNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+let score1=80
+let objectScore = Object(score)
+console.log(typeof objectScore)
+console.log(objectScore)
