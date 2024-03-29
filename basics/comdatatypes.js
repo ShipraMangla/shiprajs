05659c2bcac48c95c1a6d9b7fd248ddfc7a88a77
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-03-29 10:06:42 +0000

    comparisons

---
+++ basics/comdatatypes.js
@@ -0,0 +1,17 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log("2">1)
+// console.log("2">=1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+//equality check(==) and comparisons work in different ways. Comparisons convert the "null" to a numeric value and treats it as zero thats why line 8 got false as output and line 10 got true 
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined>=0)
+
+//===
+console.log("2"===2)
