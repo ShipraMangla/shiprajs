ad17009f86f191058480c4e2eadb7144a60a4ebc
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-03-29 09:02:37 +0000

    operations

---
+++ basics/operations.js
@@ -0,0 +1,47 @@
+let value=8
+let negative= -value;
+//console.log(negative)
+
+    // console.log(2+2)
+    // console.log(2-2)
+    // console.log(2*2)
+    // console.log(2**4)
+    // console.log(2/4)
+    // console.log(2%3)
+
+let str1="hello"
+let str2=" shipra"
+let str3=str1+str2;
+//console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log("1"+(2+2))
+// console.log(1+3+"4")
+// console.log(1+3+(3+"4"))
+
+// console.log(+true)
+// console.log(true+5)
+// console.log(+"")
+// console.log(""+5)
+
+let GameCounter=100
+GameCounter++
+//++(++GameCounter)
+console.log(GameCounter);
+//postfix=== assign the value first and then increments
+//prefix===increment first and then assigns
+
+// let x=4
+// x++;
+// let y=x;
+// console.log("x= " +x);
+// console.log("y="+y);
+
+// let x1=4
+// let y1=++x1
+// console.log("x1= " +x1);
+// console.log("y1="+y1);
+
+
