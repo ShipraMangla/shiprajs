9cf0c60302f0e11fdab56674bac9b909dfb6ce74
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-04-02 14:13:46 +0000

    datatypes summary

---
+++ basics/datawrap.js
@@ -0,0 +1,38 @@
+//There are two types of datatypes in javascript :- PRIMITVE AND NON-PRIMITVE
+//Datatypes are divided based on how they are stored in memory and how we can access those variables
+//JS is dynamically typed language as the values that the variables hold are determined at the runtime and we do not need to spcify the type explicitly
+//PRIMITVE:- These are basically call by value datatypes
+//7 types:- Number, String, Boolean. null, undefined, symbol, bigInt
+let score=30
+let ScoreV=30.45
+let name="Shipra"
+let islogged = false
+let temp=null;
+let state;
+let BigNumber=475487548546543n
+console.log([score, ScoreV, name, islogged, temp, state])
+let Value=Symbol("123")
+let Valuem=Symbol("123")
+console.log(Value===Valuem)
+console.log(Value)
+console.log(Valuem)
+
+//Non-Primitive-- Call by reference
+//Arrays, Objects, Functions
+//The datatype of all non primtive types are objects. However the datatype of function is returned as object function
+let heroes=["Wonder Woman", "Black Panther", "Captain America", "Loki"]
+console.log(heroes)
+let myObj1={
+    name: "Shipra",
+    Age: 22,
+    Title: "Engineer",
+}
+console.log(myObj1)
+
+let myfunc = function(){
+    console.log("Hello World")
+}
+console.log(myfunc)
+console.log(typeof(BigNumber))
+console.log(typeof(heroes))
+console.log(typeof(myfunc))
