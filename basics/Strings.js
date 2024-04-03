1c6679042474ea1ae65e2cda98f05d167db11b39
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-04-03 06:34:42 +0000

    Strings

---
+++ basics/Strings.js
@@ -0,0 +1,12 @@
+const name="shipra"
+const age=20;
+console.log(name+age+" that's it")
+console.log(`hello my name is ${name.toUpperCase()} and my age is ${age}`)
+const name1=new String("SHIPRA MANGLA")
+console.log(name1[2]);
+console.log(name1.__proto__);
+console.log(name1.length)
+console.log(name1.substring(0,5))
+console.log(name1.italics())
+console.log(name1.endsWith("A"));
+console.log(name1.charCodeAt(2));
