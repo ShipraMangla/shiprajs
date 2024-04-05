9a1183265bfd373073a27bbf58ab63dfdecfa1a7
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-04-05 08:56:43 +0000

    numberandmaths

--- basics/nums.js
+++ basics/nums.js
@@ -1,6 +1,25 @@
 const score=100
 console.log(score)
 const s=new Number(200)
-console.log(s)
-console.log(s.toString().length)
-console.log(s.toFixed(2))
+// console.log(s)
+// console.log(s.toString().length)
+// console.log(s.toFixed(2))
+let otherNum=123.9954
+// console.log(otherNum.toPrecision(3));
+// console.log(s.toExponential())
+let Hundred=100000000
+// console.log(Hundred.toLocaleString("en-IN"));
+// console.log(typeof(s.valueOf()));
+// console.log(typeof(s));
+//*****************Math***************** */
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.8));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9));
+// console.log(Math.pow(5,3))
+console.log(Math.random())
+console.log(Math.floor((Math.random()*10)+1))
+let min=11
+let max=20
+console.log(Math.floor((Math.random()*(max-min+1))+min))
