48898ce1106fbcf4330bfb2d648abebce0f4eafc
ShipraMangla <102975569+ShipraMangla@users.noreply.github.com>
2024-06-05 04:58:18 +0000

    dowhile

---
+++ iteratins/while.js
@@ -0,0 +1,23 @@
+//while
+let index=0
+while (index<=10) {
+    console.log(`Value is ${index}`);
+    index++
+}
+let myArray=["shipra", "arya","moksha"]
+let arr=0
+while (arr<myArray.length) {
+    console.log(`name is ${myArray[arr]}`);
+    arr++
+}
+
+let score=0
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
+let score1=11
+do {
+    console.log(`score actually is ${score}`);
+    score++
+} while (score<=10);
